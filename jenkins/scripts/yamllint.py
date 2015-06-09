069f263e373748e6c10229b42730be9916f274a0
David Ramirez <dramirezp@hp.com>
2015-06-09 11:53:29 -0600

    Adding CI to Simulator config project. Changed the extension of the file
    Change-Id: Ibc1162141348cb777a884f3214e4c64e18d92403
    Signed-off-by: David Ramirez <dramirezp@hp.com>

--- jenkins/scripts/yamllint.py
+++ jenkins/scripts/yamllint.py
@@ -4,8 +4,14 @@ import sys
 import yaml
 
 if len(sys.argv) < 2:
-  print 'Missing file to lint'
-  sys.exit(1)
+    print 'Missing file to lint'
+    sys.exit(1)
 
-yaml.load( open(sys.argv[1], 'r'), Loader=yaml.CLoader)
-print 'YAML syntax OK'
+i = 1
+while i < len(sys.argv):
+    try:
+	yaml.load( open(sys.argv[i], 'r'), Loader=yaml.CLoader)
+	print 'YAML: Syntax OK in file: '+ sys.argv[i]
+    except:
+	print "YAML: Invalid FAIL in file " + sys.argv[i]
+    i+=1
