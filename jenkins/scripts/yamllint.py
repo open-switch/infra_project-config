85d3dc5f23939cfc3619674da384086fb1694ab0
himabindu <himabindu.melachuru@hpe.com>
2016-06-08 11:41:25 -0700

    Adding a trailing whitespace check to job config yaml files using yamllint
    Change-Id: If045a3b104362db2726e1f130b6a8e862b563237

--- jenkins/scripts/yamllint.py
+++ jenkins/scripts/yamllint.py
@@ -2,11 +2,26 @@
 
 import sys
 import yaml
-
+import string
+from yamllint.linter import LintProblem
 if len(sys.argv) < 2:
     print 'Missing file to lint'
     sys.exit(1)
 
+ID = 'trailing-spaces'
+TYPE = 'line'
+
+
+def check(conf, line):
+    if line.end == 0:
+        return
+pos = line.end
+    while line.buffer[pos - 1] in string.whitespace and pos > line.start:
+        pos -= 1
+
+    if pos != line.end and line.buffer[pos] in ' \t':
+        yield LintProblem(line.line_no, pos - line.start + 1,
+                          'trailing spaces')
 i = 1
 while i < len(sys.argv):
     try:
